28c13484428775141df49c6d783c606bd9d1389b
Damian Cichosz <damcichosz@gmail.com>
2024-11-24 18:41:40 +0100

    Add wsdl function for xml transformation

--- app/controllers/api/v1/map_controller.rb
+++ app/controllers/api/v1/map_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::MapController < ApplicationController
   require 'mini_magick'
+  require 'savon'
 
   def cutout
     x1, y1, x2, y2 = params[:x1].to_i, params[:y1].to_i, params[:x2].to_i, params[:y2].to_i
@@ -13,5 +14,54 @@ class Api::V1::MapController < ApplicationController
     render json: { image: Base64.encode64(cropped_map.to_blob) }
   rescue => e
     render json: { error: e.message }, status: :unprocessable_entity
+  end
+
+  def wsdl
+    render xml: build_wsdl
+  end
+
+  private
+
+  def build_wsdl
+    <<~WSDL
+      <?xml version="1.0" encoding="UTF-8"?>
+      <definitions xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"
+                   xmlns:tns="http://your.namespace.com/soap"
+                   xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+                   xmlns:wsdl="http://schemas.xmlsoap.org/wsdl/">
+        <message name="getMapFragmentRequest">
+          <part name="leftTopX" type="xsd:int"/>
+          <part name="leftTopY" type="xsd:int"/>
+          <part name="rightBottomX" type="xsd:int"/>
+          <part name="rightBottomY" type="xsd:int"/>
+        </message>
+        <message name="getMapFragmentResponse">
+          <part name="image" type="xsd:string"/>
+        </message>
+        <portType name="MapServicePortType">
+          <operation name="getMapFragment">
+            <input message="tns:getMapFragmentRequest"/>
+            <output message="tns:getMapFragmentResponse"/>
+          </operation>
+        </portType>
+        <binding name="MapServiceBinding" type="tns:MapServicePortType">
+          <soapenv:binding style="rpc" transport="http://schemas.xmlsoap.org/soap/http"/>
+          <operation name="getMapFragment">
+            <soapenv:operation soapAction="urn:getMapFragment"/>
+            <input>
+              <soapenv:body use="encoded" namespace="http://your.namespace.com/soap"/>
+            </input>
+            <output>
+              <soapenv:body use="encoded" namespace="http://your.namespace.com/soap"/>
+            </output>
+          </operation>
+        </binding>
+        <service name="MapService">
+          <port name="MapServicePort" binding="tns:MapServiceBinding">
+            <soapenv:address location="http://localhost:3000/api/v1/map/cutout"/>
+          </port>
+        </service>
+      </definitions>
+    WSDL
   end
 end
